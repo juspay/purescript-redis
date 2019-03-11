643b9f176242cdd9472a1929d98a1bfa0b5175ea
aravind mallapureddy <aravind.mallapureddy@juspay.in>
2019-03-11 17:00:30 +0530

    added different test folders

--- test/Main.purs
+++ test/Main.purs
@@ -7,6 +7,8 @@ import Control.Monad.Aff (Aff, launchAff)
 import Control.Monad.Eff (Eff)
 import Data.Options (options, (:=))
 import Debug.Trace (spy, traceShow)
+import Test.Multi (multiTest)
+import Test.Queue (queueTest)
 
 foreign import startContext :: forall e a. String -> Eff e a -> Eff e Unit
 
@@ -14,27 +16,8 @@ startTest :: forall e. Aff _ Unit
 startTest = do
     let cacheOpts = C.host := "127.0.0.1" <> C.port := 6379 <> C.db := 0 <> C.socketKeepAlive := true
     cacheConn <- C.getConn cacheOpts
-    {-- v <- C.setKey cacheConn "testing" "100" --}
-    {-- v0 <- C.lpop cacheConn "test-queue" --}
-    {-- _ <- traceShow v0 \_ -> pure unit --}
-    {-- v1 <- C.rpush cacheConn "test-queue" "hi" --}
-    {-- _ <- traceShow v1 \_ -> pure unit --}
-    {-- v2 <- C.lpop cacheConn "test-queue" --}
-    {-- _ <- traceShow v2 \_ -> pure unit --}
-    {-- v3 <- C.lpop cacheConn "test-queue" --}
-    {-- _ <- traceShow v3 \_ -> pure unit --}
-    multi <- C.getMulti cacheConn
-    multi <- C.setHashMulti "myhash" "firstKey" "100" multi
-    val <- C.getHashKeyMulti "myhash" "firstKey" multi
-    {-- val <- C.setKeyMulti "tt" "100" multi >>= C.setexKeyMulti "testing" "200" "1000" >>= C.incrMulti "testing" >>= C.getKeyMulti "tt" --} 
-    val <- C.exec multi
-    _ <- pure $ spy val
-    -- v <- C.setex cacheConn "talk" "i am awesome" "10000"
-    -- l <- C.enqueue cacheConn "DBACTIONS" "SELCT * FROM CUSTOMERS;"
-    -- pop <- C.dequeue cacheConn "DBACTIONS"
-    {-- peek <- C.getQueueIdx cacheConn "DBACTIONS" 0 --}
-    {-- _ <- pure $ spy peek --}
-    {-- _ <- pure $ spy $ "It worked" --}
+    multiTest cacheConn
+    queueTest cacheConn
     pure unit
 
 main :: forall e. Eff _ Unit
