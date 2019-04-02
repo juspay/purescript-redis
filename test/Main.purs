fb5c5528a2734f25ee46b730de44d031ad4a4bf9
Arun Raghavan <arun.raghavan@juspay.in>
2019-04-02 18:56:37 +0530

    Drop unnecessary decoration in logs

--- test/Main.purs
+++ test/Main.purs
@@ -2,17 +2,14 @@ module Test.Main where
 
 import Prelude
 
-import Cache (CACHE, db, exec, getConn, getHashKey, getHashKeyMulti, getKey, getKeyMulti, getMulti, host, incr, incrMulti, lpop, port, rpush, setHash, setHashMulti, setKey, setKeyMulti, setMulti, setex, setexKeyMulti, socketKeepAlive) as C
+import Cache (db, getConn, host, port, socketKeepAlive) as C
 import Control.Monad.Aff (Aff, launchAff)
 import Control.Monad.Eff (Eff)
-import Data.Options (options, (:=))
-import Debug.Trace (spy, traceShow)
+import Data.Options ((:=))
 import Test.Multi (multiTest)
 import Test.Queue (queueTest)
 
-foreign import startContext :: forall e a. String -> Eff e a -> Eff e Unit
-
-startTest :: forall e. Aff _ Unit
+startTest :: Aff _ Unit
 startTest = do
     let cacheOpts = C.host := "127.0.0.1" <> C.port := 6379 <> C.db := 0 <> C.socketKeepAlive := true
     cacheConn <- C.getConn cacheOpts
@@ -20,5 +17,5 @@ startTest = do
     queueTest cacheConn
     pure unit
 
-main :: forall e. Eff _ Unit
-main = startContext "awesome" (launchAff startTest) *> pure unit
+main :: Eff _ Unit
+main = launchAff startTest *> pure unit
