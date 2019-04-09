b54a76b61103b57e51d02a3f2b0a9178556de315
Arun Raghavan <arun.raghavan@juspay.in>
2019-04-09 11:22:45 +0530

    Move Multi tests to use Test.Spec
    This actually sets up some automated testing of values, and should be
    expanded to be thorough.

--- test/Main.purs
+++ test/Main.purs
@@ -5,19 +5,22 @@ import Prelude
 import Cache (db, getConn, host, port, socketKeepAlive) as C
 import Control.Monad.Aff (Aff, launchAff)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
 import Data.Options ((:=))
 import Test.Multi (multiTest)
 import Test.Queue (queueTest)
+import Test.Spec.Reporter (consoleReporter)
+import Test.Spec.Runner (run)
 import Test.Stream (streamTest)
 
 startTest :: Aff _ Unit
 startTest = do
     let cacheOpts = C.host := "127.0.0.1" <> C.port := 6379 <> C.db := 0 <> C.socketKeepAlive := true
     cacheConn <- C.getConn cacheOpts
-    multiTest cacheConn
     queueTest cacheConn
-    streamTest cacheConn
-    pure unit
+    liftEff $ run [consoleReporter] do
+       multiTest cacheConn
+       streamTest cacheConn
 
 main :: Eff _ Unit
 main = launchAff startTest *> pure unit
--- test/Multi.purs
+++ test/Multi.purs
@@ -1,21 +1,26 @@
 module Test.Multi where
 
-
 import Prelude
 
 import Cache (CacheConn)
 import Cache.Multi (execMulti, getHashKeyMulti, newMulti, setHashMulti) as C
-import Control.Monad.Aff (Aff, launchAff)
-import Control.Monad.Eff (Eff)
-import Data.Options (options, (:=))
-import Debug.Trace (spy, traceShow)
+import Data.Array (index)
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
+import Test.Spec (Spec, describe, it)
+import Test.Spec.Assertions (fail)
 
-multiTest :: CacheConn -> Aff _ Unit
-multiTest cacheConn = do
-    let multi   = C.newMulti cacheConn
-        multi'  = C.setHashMulti "myhash" "firstKey" "100" multi
-        multi'' = C.getHashKeyMulti "myhash" "firstKey" multi'
-    {-- val <- C.setKeyMulti "tt" "100" multi >>= C.setexKeyMulti "testing" "200" "1000" >>= C.incrMulti "testing" >>= C.getKeyMulti "tt" --}
-    val <- C.execMulti multi''
-    _ <- pure $ spy val
-    pure unit
+multiTest :: CacheConn -> Spec _ Unit
+multiTest cacheConn =
+  describe "Multi" do
+     it "works" do
+        let multi   = C.newMulti cacheConn
+            multi'  = C.setHashMulti "myhash" "firstKey" "100" multi
+            multi'' = C.getHashKeyMulti "myhash" "firstKey" multi'
+        {-- val <- C.setKeyMulti "tt" "100" multi >>= C.setexKeyMulti "testing" "200" "1000" >>= C.incrMulti "testing" >>= C.getKeyMulti "tt" --}
+        val <- C.execMulti multi''
+        case (flip index 1 <$> val) of
+             Right (Just "100") -> pure unit
+             Right (Just v)     -> fail $ "Bad value: " <> v
+             Right Nothing      -> fail $ "Did not get value"
+             Left err           -> fail $ show err
