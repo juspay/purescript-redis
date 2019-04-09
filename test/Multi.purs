43d23a9dc5e37cb9972f6167bb747cc092097288
Arun Raghavan <arun.raghavan@juspay.in>
2019-04-09 14:34:19 +0530

    Write the Multi test so it expresses better
    Using the (#) operator lets us write things in a more easy-to-read
    manner like we would have been with (>>=) when it was in the Aff monad.

--- test/Multi.purs
+++ test/Multi.purs
@@ -14,11 +14,11 @@ multiTest :: CacheConn -> Spec _ Unit
 multiTest cacheConn =
   describe "Multi" do
      it "works" do
-        let multi   = C.newMulti cacheConn
-            multi'  = C.setHashMulti "myhash" "firstKey" "100" multi
-            multi'' = C.getHashKeyMulti "myhash" "firstKey" multi'
+        let multi = C.newMulti cacheConn
+                  # C.setHashMulti "myhash" "firstKey" "100"
+                  # C.getHashKeyMulti "myhash" "firstKey"
         {-- val <- C.setKeyMulti "tt" "100" multi >>= C.setexKeyMulti "testing" "200" "1000" >>= C.incrMulti "testing" >>= C.getKeyMulti "tt" --}
-        val <- C.execMulti multi''
+        val <- C.execMulti multi
         case (flip index 1 <$> val) of
              Right (Just "100") -> pure unit
              Right (Just v)     -> fail $ "Bad value: " <> v
