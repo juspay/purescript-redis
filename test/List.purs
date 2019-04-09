62597d91c6fad0ef87a8edf5775dc5c4f1700160
Arun Raghavan <arun.raghavan@juspay.in>
2019-04-09 16:44:12 +0530

    Rename Queue as List in tests
    Rename since that is what the data structure is.

--- test/List.purs
+++ test/List.purs
@@ -1,4 +1,4 @@
-module Test.Queue where
+module Test.List where
 
 import Prelude
 
@@ -9,15 +9,15 @@ import Data.Maybe (Maybe(..))
 import Data.Time.Duration (Milliseconds(..))
 import Debug.Trace (spy, traceShow)
 
-queueTest :: CacheConn -> Aff _ Unit
-queueTest cacheConn = do
-    v0 <- C.lpop cacheConn "test-queue"
+listTest :: CacheConn -> Aff _ Unit
+listTest cacheConn = do
+    v0 <- C.lpop cacheConn "test-list"
     _ <- traceShow v0 \_ -> pure unit
-    v1 <- C.rpush cacheConn "test-queue" "hi"
+    v1 <- C.rpush cacheConn "test-list" "hi"
     _ <- traceShow v1 \_ -> pure unit
-    v2 <- C.lpop cacheConn "test-queue"
+    v2 <- C.lpop cacheConn "test-list"
     _ <- traceShow v2 \_ -> pure unit
-    v3 <- C.lpop cacheConn "test-queue"
+    v3 <- C.lpop cacheConn "test-list"
     _ <- traceShow v3 \_ -> pure unit
     v <- C.set cacheConn "talk" "i am awesome" (Just $ Milliseconds 10000.0) NoOptions
     l <- C.rpush cacheConn "DBACTIONS" "SELCT * FROM CUSTOMERS;"
--- test/Main.purs
+++ test/Main.purs
@@ -8,7 +8,7 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Data.Options ((:=))
 import Test.Multi (multiTest)
-import Test.Queue (queueTest)
+import Test.List (listTest)
 import Test.Spec.Reporter (consoleReporter)
 import Test.Spec.Runner (run)
 import Test.Stream (streamTest)
@@ -17,7 +17,7 @@ startTest :: Aff _ Unit
 startTest = do
     let cacheOpts = C.host := "127.0.0.1" <> C.port := 6379 <> C.db := 0 <> C.socketKeepAlive := true
     cacheConn <- C.getConn cacheOpts
-    queueTest cacheConn
+    listTest cacheConn
     liftEff $ run [consoleReporter] do
        multiTest cacheConn
        streamTest cacheConn
