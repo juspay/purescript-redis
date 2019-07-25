afdd65a54519e1c68ca99a3dc0452d0e3bb82510
Nikith Shetty <nikith.shetty@juspay.in>
2019-07-25 23:31:23 +0530

    added Sorted set apis

---
+++ src/Cache/SortedSet.js
@@ -0,0 +1,57 @@
+/*
+* Copyright (c) 2012-2019 "JUSPAY Technologies"
+* JUSPAY Technologies Pvt. Ltd. [https://www.juspay.in]
+*
+* This file is part of JUSPAY Platform.
+*
+* JUSPAY Platform is free software: you can redistribute it and/or modify
+* it for only educational purposes under the terms of the GNU Affero General
+* Public License (GNU AGPL) as published by the Free Software Foundation,
+* either version 3 of the License, or (at your option) any later version.
+* For Enterprise/Commerical licenses, contact <info@juspay.in>.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  The end user will
+* be liable for all damages without limitation, which is caused by the
+* ABUSE of the LICENSED SOFTWARE and shall INDEMNIFY JUSPAY for such
+* damages, claims, cost, including reasonable attorney fee claimed on Juspay.
+* The end user has NO right to claim any indemnification based on its use
+* of Licensed Software. See the GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <https://www.gnu.org/licenses/agpl.html>.
+*/
+
+"use strict";
+
+exports["zaddJ"] = function (client, key, options, returnOptions, argsObj) {
+  var allArgs = [key];
+
+  if (options != "") {
+    allArgs.push(options);
+  }
+
+  if (returnOptions != "") {
+    allArgs.push(returnOptions);
+  }
+
+  Object.keys(argsObj).map(function(key) {
+    allArgs.push(argsObj[key]); //score
+    allArgs.push(key); //member
+  });
+
+  return client.zadd(allArgs);
+}
+
+exports["zrangeJ"] = function (client, key, start, stop) {
+  return client.zrange(key, start, stop);
+}
+
+exports["zincrbyJ"] = function (client, key, increment, member) {
+  return client.zincrby(key, increment, member);
+}
+
+exports["zremJ"] = function (client, key, memberArr) {
+  return client.zrem(key, memberArr);
+}
---
+++ src/Cache/SortedSet.purs
@@ -0,0 +1,59 @@
+module Cache.SortedSet
+(
+  zadd
+, zrange
+, zincrby
+, zrem
+, ReturnOptions(..)
+, Score
+, Member
+, Key
+, SortedSetItem
+) where
+
+import Prelude
+
+import Cache.Internal (readStringMaybe)
+import Cache.Types (class CacheConn, SetOptions)
+import Control.Monad.Except (runExcept)
+import Control.Promise (Promise, toAff)
+import Data.Array (foldl, (:))
+import Data.Either (Either, hush)
+import Data.Function.Uncurried (Fn3, Fn4, Fn5, runFn3, runFn4, runFn5)
+import Data.Maybe (Maybe)
+import Data.Number (fromString)
+import Data.Tuple (Tuple(..))
+import Effect.Aff (Aff, Error, attempt)
+import Foreign (Foreign, readNumber)
+import Foreign.Object (Object, empty, insert)
+
+foreign import zaddJ :: forall a. Fn5 a String String String (Object String) (Promise Int)
+foreign import zrangeJ :: forall a. Fn4 a String Int Int (Promise (Array Foreign))
+foreign import zincrbyJ :: forall a. Fn4 a String Number String (Promise String)
+foreign import zremJ :: forall a. Fn3 a String (Array String) (Promise Int)
+
+data ReturnOptions = Default
+                | Changed
+
+instance showReturnOptions :: Show ReturnOptions where
+  show Default  = ""
+  show Changed = "CH"
+
+type Score = Number
+type Member = String
+type Key = String
+type SortedSetItem = Tuple Score Member
+
+zadd :: forall a. CacheConn a => a -> Key -> SetOptions -> ReturnOptions -> Array SortedSetItem -> Aff (Either Error Int)
+zadd cacheConn key options returnOptions values = attempt <<< toAff $ runFn5 zaddJ cacheConn key (show options) (show returnOptions) getValuesAsObj
+  where
+    getValuesAsObj = foldl (\acc (Tuple field val) -> insert val (show field) acc) empty values --making val as key of the object as member is unique and score can be repeated
+
+zrange :: forall a. CacheConn a => a -> Key -> Int -> Int -> Aff (Either Error (Array (Maybe String)))
+zrange cacheConn key start stop = attempt <<< map (map readStringMaybe) <<< toAff $ runFn4 zrangeJ cacheConn key start stop
+
+zincrby :: forall a. CacheConn a => a -> Key -> Number -> Member -> Aff (Either Error (Maybe Number))
+zincrby cacheConn key increment member = attempt <<< map fromString <<< toAff $ runFn4 zincrbyJ cacheConn key increment member
+
+zrem :: forall a. CacheConn a => a -> Key -> Array Member -> Aff (Either Error Int)
+zrem cacheConn key memberArr = attempt <<< toAff $ runFn3 zremJ cacheConn key memberArr
