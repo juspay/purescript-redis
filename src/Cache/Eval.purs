e3d83aa23a0d3ccfbea369cb6bb6675cfcaa6d1b
Nikith Shetty <nikith.shetty@juspay.in>
2019-08-05 22:17:22 +0530

    changed return type of defineCommand

--- src/Cache/Eval.purs
+++ src/Cache/Eval.purs
@@ -15,10 +15,10 @@ import Effect.Aff (Aff, Error, attempt)
 import Effect.Class (liftEffect)
 import Foreign (Foreign)
 
-foreign import defineCommandJ :: forall a. Fn4 a String Int String (Effect Foreign)
+foreign import defineCommandJ :: forall a. Fn4 a String Int String (Effect Unit)
 foreign import runCommandJ :: forall a. Fn4 a String (Array String) (Array String) (Promise Foreign)
 
-defineCommand :: forall a. CacheConn a => a -> String -> Int -> String -> Aff (Either Error Foreign)
+defineCommand :: forall a. CacheConn a => a -> String -> Int -> String -> Aff (Either Error Unit)
 defineCommand cacheConn scriptName numberOfKeys script = attempt <<< liftEffect $ runFn4 defineCommandJ cacheConn scriptName numberOfKeys script
 
 runCommand :: forall a. CacheConn a => a -> String -> Array String -> Array String -> Aff (Either Error Foreign)
