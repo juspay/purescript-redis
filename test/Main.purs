4449bec77dc736c11a8290ce0062296d98a3c414
Shubham Kumar <hello@shubhkumar.in>
2020-08-19 16:00:57 +0530

    Added Cluster back

--- test/Main.purs
+++ test/Main.purs
@@ -3,15 +3,15 @@ module Test.Main where
 import Prelude
 
 import Cache as C
+import Cache.Cluster as Cluster
 import Data.Either (Either(..))
 import Data.Int (fromString)
 import Data.Maybe (fromMaybe, maybe)
 import Data.Options (assoc, (:=))
-import Debug.Trace (spy)
 import Effect (Effect)
 import Effect.Aff (Aff, launchAff)
 import Effect.Class (liftEffect)
-import Node.Process
+import Node.Process (lookupEnv)
 import Test.Basic (basicTest)
 import Test.Eval (evalTest)
 import Test.Hash (hashTest)
@@ -33,7 +33,7 @@ startTest = do
     mpassword <- (liftEffect $ lookupEnv "REDIS_PASSWORD")
     let cacheOpts = C.host := host <> C.port := port <> C.db := db <> C.socketKeepAlive := true <> maybe mempty (assoc C.password) mpassword
     eCacheConn <- C.newConn cacheOpts
-    -- eClusterConn <- Cluster.newClusterConn [{ host: "127.0.0.1", port: 7000 }] mempty
+    eClusterConn <- Cluster.newClusterConn [{ host: host, port: port }] mempty
     runSpec [consoleReporter] do
        describe "Simple connection"
          case eCacheConn of
@@ -50,19 +50,19 @@ startTest = do
               it "fails" do
                  fail $ show err
 
-       -- describe "Cluster connection"
-       --   case eClusterConn of
-       --     Right cacheConn -> do
-       --        basicTest cacheConn
-       --        listTest cacheConn
-       --        hashTest cacheConn
-       --        multiTest cacheConn
-       --        streamTest cacheConn
-       --        sortedSetTest cacheConn
-       --        evalTest cacheConn
-       --     Left err        -> do
-       --        it "fails" do
-       --           fail $ show err
+       describe "Cluster connection"
+         case eClusterConn of
+           Right cacheConn -> do
+              basicTest cacheConn
+              listTest cacheConn
+              hashTest cacheConn
+              multiTest cacheConn
+              streamTest cacheConn
+              sortedSetTest cacheConn
+              evalTest cacheConn
+           Left err        -> do
+              it "fails" do
+                 fail $ show err
 
 main :: Effect Unit
 main = launchAff startTest *> pure unit
